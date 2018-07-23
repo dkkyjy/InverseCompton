d31588b99cc51a9da52d77691cc5d993b423d8a0
dunakk <duankk@pmo.ac.cn>
2018-07-23 10:48:02 +0800

    add some commit

--- InverseCompton.c
+++ InverseCompton.c
@@ -1,6 +1,10 @@
 #include <math.h>
 #include <stdio.h>
 #include "InverseCompton.h"
+/*
+ * Follow the paper A&A 367, 809-825 (2001)
+ * THe multifrequency emission of Mrk 501 from radio to TeV gamma-rays
+ */ 
 
 double c = 1.00e+10;
 double h = 6.63e-27
@@ -8,27 +12,21 @@ double m_e = 9.11e-28;
 double r_e = 2.818e-13;
 
 //gamma * m_e * c**2 is the energy of electron;
-//E0 * m_e * c**2 is the energy of soft photon;
-//E * m_e * c**2 is the energy of scattered photon.
+//Es * m_e * c**2 is the energy of Synchrotron photon;
+//Ec * m_e * c**2 is the energy of InverseCompton photon.
 
-double Kappa(double gamma, double E0, double E){
-    return E / (4 * E0 * gamma * (gamma - E));
+double Kappa(double Ec, double gamma, double Es){
+    return Ec / (4 * Es * gamma * (gamma - Ec));
 }
 
-double C(double gamma, double E0, double E){
-    double k = Kappa(gamma, E, E0);
-    return 2 * M_PI * r_e * r_e * c / (gamma * gamma * E0)\
-    * (2 * k * log(k) + (1+2*k)*(1-k) + (4*E0*gamma*k)*(4*E0*gamma*k)*(1-k) / (2 * (1+4*E0*gamma*k)));
+double C(double Ec, double gamma, double Es){
+    double k = Kappa(Ec, gamma, Es);
+    return 2 * M_PI * r_e * r_e * c / (gamma * gamma * Es)\
+    * (2 * k * log(k) + (1+2*k)*(1-k) + (4*Es*gamma*k)*(4*Es*gamma*k) / (2 * (1+4*Es*gamma*k)) * (1-k));
 }
 
-double n_E0(double E0, double j, double k, double R){
-    double C_corr = 0.75;
-    return 4 * M_PI / (h * c * E0) * C_corr * j / k * (1 - exp(-k * R));
-}
-
-double q(double gamma, double E0, double E){
-    double n = n_E0(E0, j, k, R);
-    return ;
+double n_Es(double Es, double j, double k, double R){
+    return (3./4) * (4*M_PI) / (h*c*Es) * (j/k) * (1 - exp(-k*R));
 }
 
 double Emin(double gamma, double E0){
@@ -39,10 +37,20 @@ double Emax(double gamma, double E0){
     return gamma * (4 * E0 * gamma) / (1 + 4 * E0 * gamma);
 }
 
-double j(double E){
-    return h / (4 * M_PI) * E * q;
+double q(double Ec, double gamma, double E0){
+    double n_E0 = n_E0(E0, j, k, R);
+    double n_gamma = PowerLaw(gamma, K, p, gamma_min, gamma_max);
+    
+    double C = C(gamma, E0, E);
+
+    return ;
 }
 
-double nu(double E){
-    return E * m_e * c * c / h;
+double j(double Ec){
+    double q = q(Ec);
+    return h / (4 * M_PI) * Ec * q;
+}
+
+double nu(double Es){
+    return Es * m_e * c * c / h;
 }
